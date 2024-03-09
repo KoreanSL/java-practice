c755fae33f641b13be683e2c64b26a952415cf61
KoreanSL <koreansl357@gmail.com>
2024-03-10 04:04:24 +0900

    트리

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("hello world!");
+            // 공백 출력
+            for (int j = 0; j < 10 - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
